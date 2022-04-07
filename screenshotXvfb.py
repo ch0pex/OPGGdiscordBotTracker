fdf4fa74448fdaeb4c34bd4d764c9d02cde5acda
alvarocabrerabarrio <alvarocabrerabarrio@gmail.com>
2022-04-07 18:52:13 +0000

    optimized for discord

--- screenshotXvfb.py
+++ screenshotXvfb.py
@@ -10,8 +10,8 @@ BINARY_LOCATION = "/usr/bin/google-chrome"
 
 
 DISPLAY_VISIBLE = 0
-DISPLAY_WIDTH = 800
-DISPLAY_HEIGHT = 3000
+DISPLAY_WIDTH = 1000
+DISPLAY_HEIGHT = 1000
 def screenshot_op_gg(summoner):
 
     # start display
@@ -30,7 +30,7 @@ def screenshot_op_gg(summoner):
     print("current_url", driver.current_url)
     title=driver.title
     print("title: ", title)
-
+    time.sleep(5)
     # 1.2 take a screenshot
     filename = f"screenshot/{summoner}.png"
     driver.save_screenshot(filename)
@@ -39,24 +39,4 @@ def screenshot_op_gg(summoner):
     display.stop()
     return filename
 
-    # 1.3 click link to signin options page
-"""    WAIT_ELEMENT = 30
-    XPATH_VALUE = "//*[@id=\"imdbHeader\"]/div[2]/div[5]/a/div"
-    x = WebDriverWait(driver, WAIT_ELEMENT).until(EC.presence_of_element_located((By.XPATH, XPATH_VALUE)))
-    outerHTML = x.get_attribute('outerHTML')
-    print(outerHTML)
-    time.sleep(5)
-    x.click()
 
-    # 2.1 driver is now on the signin options page
-    print("\n", "signin options page --------->")
-    print("current_url", driver.current_url)
-    title=driver.title
-    print("title: ", title)
-
-    # 2.2 take a screenshot
-    time.sleep(5)
-    filename = f"/home/piuser/Desktop/tutorial/sel/screenshot/{title}.png"
-    driver.save_screenshot(filename)"""
-
-    # close browser and quit driver
